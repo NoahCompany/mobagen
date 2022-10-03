e9b711e73764b6a09751b5caac62003946960b6a
Noah Thompson <noah.thompson@mymail.champlain.edu>
2022-10-03 14:04:30 -0400

    Started making vector for searched and potential next locations

--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -1,13 +1,24 @@
 #ifndef AGENT_H
 #define AGENT_H
 #include "Point2D.h"
+#include <unordered_map>
+using namespace std;
 
 class World;
 
-class Agent {
+struct queueEntry
+{
+  Point2D position = Point2D(0,0);
+  int totalWeight;
+};
+
+class Agent 
+{
 public:
   explicit Agent()= default;;
   virtual Point2D Move(World*)=0;
+  vector<queueEntry> searched;
+  vector<queueEntry> neighbor;
 };
 
 #endif  // AGENT_H
--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -1,11 +1,45 @@
 #include "Cat.h"
 #include "World.h"
 #include <stdexcept>
+#include <vector>
+using namespace std;
 
-Point2D Cat::Move(World* world) {
-  auto rand = Random::Range(0,5);
+Point2D Cat::Move(World* world) 
+{
+  neighbor.clear();
+  queueEntry current;
+  queueEntry next;
+  auto choice = 0;
   auto pos = world->getCat();
-  switch(rand){
+  current.position = pos;
+  if (searched.size() > 0)
+    current.totalWeight = searched[searched.size()-1].totalWeight + 1;
+  else 
+  {
+    current.totalWeight = 1;
+  }
+  searched.push_back(current);
+  next.position = World::NE(pos);
+  next.totalWeight = current.totalWeight + 1;
+  neighbor.push_back(next);
+  next.position = World::NW(pos);
+  next.totalWeight = current.totalWeight + 1;
+  neighbor.push_back(next);
+  next.position = World::E(pos);
+  next.totalWeight = current.totalWeight + 1;
+  neighbor.push_back(next);
+  next.position = World::W(pos);
+  next.totalWeight = current.totalWeight + 1;
+  neighbor.push_back(next);
+  next.position = World::SE(pos);
+  next.totalWeight = current.totalWeight + 1;
+  neighbor.push_back(next);
+  next.position = World::SW(pos);
+  next.totalWeight = current.totalWeight + 1;
+  neighbor.push_back(next);
+  return neighbor[neighbor.size()-3].position;
+  switch(choice)
+  {
     case 0:
       return World::NE(pos);
     case 1:
@@ -21,4 +55,5 @@ Point2D Cat::Move(World* world) {
     default:
       throw "random out of range";
   }
+    
 }
