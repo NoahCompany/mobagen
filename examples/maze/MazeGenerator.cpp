b66caeddb32d02e931db2d4892a72785c070f3b4
Noah Thompson <noah.thompson@mymail.champlain.edu>
2022-10-27 14:05:20 -0400

    Started work

--- examples/maze/MazeGenerator.cpp
+++ examples/maze/MazeGenerator.cpp
@@ -1,7 +1,10 @@
 #include "MazeGenerator.h"
 #include "World.h"
+#include <stdlib.h>
+#include <time.h>
 
-bool MazeGenerator::Step(World* world) {
+bool MazeGenerator::Step(World* world) 
+{
   return false;
 }
 void MazeGenerator::Clear(World* world) {}
